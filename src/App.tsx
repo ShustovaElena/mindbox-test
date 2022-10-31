bd3bf9c58c5197643aca511bd799f5112cfaa02e
Elena Shustova <alenyf-ka@mail.ru>
2022-10-31 14:04:27 +0300

    feat: add task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Input } from './components/Input';
+import { Task } from './components/Task';
 
 function App() {
   return (
     <>
         <h1 className="header">TODOS</h1>
         <Input />
+        <Task label={'Сделать тестовое'}/>
     </>
   );
 }
