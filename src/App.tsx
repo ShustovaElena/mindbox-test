404a300dd0ae4d25f7bcf06200eafaade6a61c1d
Elena Shustova <alenyf-ka@mail.ru>
2022-10-31 12:32:43 +0300

    feat: create header, textfield

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Input } from './components/Input';
+
+function App() {
+  return (
+    <>
+        <h1 className="header">TODOS</h1>
+        <Input />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import TextField from '@mui/material/TextField';
+import React from 'react';
+
+export const Input = () => {
+    return (
+        <TextField id="input" label="What needs to be done?" variant="outlined" fullWidth />
+    );
+};
